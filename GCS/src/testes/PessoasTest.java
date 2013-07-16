c4206799952224d5198d11336333d615fc734edc
giancarllo <giancarllo@giancarllo-virtual-machine>
2013-07-15 22:29:22 -0300

    Commit 1 - Branch teste - TestPessoa - MudarNome().

--- GCS/src/testes/PessoasTest.java
+++ GCS/src/testes/PessoasTest.java
@@ -14,5 +14,13 @@ public class PessoasTest {
 		Assert.assertEquals("Ahmed Angel", test.getNome());
 		Assert.assertEquals(24, test.getIdade());
 	}
+	
+	@Test
+	public void MudarNome(){
+		Pessoa test = new Pessoa("Ahmed Angel", 24);
+		test.setNome("Sora");
+		Assert.assertNotEquals("Ahmed Angel", test.getNome());
+		Assert.assertEquals("Sora", test.getNome());
+	}
 
 }

dbd7a6d73c58b31f3a195039679f086d577be9d2
giancarllo <giancarllo@giancarllo-virtual-machine>
2013-07-15 22:13:59 -0300

    Commit 2 - Classe de teste Pessoa criada!

---
+++ GCS/src/entidades/Pessoa.java
@@ -0,0 +1,30 @@
+package entidades;
+
+public class Pessoa {
+
+	private String nome;
+	private int idade;
+
+	public Pessoa(String nome, int idade) 
+	{	
+		this.setNome(nome);
+		this.setIdade(idade);	
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+}
---
+++ GCS/src/testes/PessoasTest.java
@@ -0,0 +1,18 @@
+package testes;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import entidades.Pessoa;
+
+public class PessoasTest {
+
+
+	@Test
+	public void criarPessoa() {
+		Pessoa test = new Pessoa("Ahmed Angel", 24);
+		Assert.assertEquals("Ahmed Angel", test.getNome());
+		Assert.assertEquals(24, test.getIdade());
+	}
+
+}
